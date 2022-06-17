1787c7558478a7222cb08f65dfc5c8e2769d8a9d
AlexeyPolskiy <flyouss@mail.ru>
2022-06-17 22:34:23 +0500

    enter the commit message for your changes. Lines starting

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27.
+// 5 -> 1, 8, 27, 64, 125
+
+int prompt(string message)
+{
+    System.Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse(number);
+    return numberInt;
+}
+int numberInt = prompt("Введите число N -> ");
+for (int i = 1; i <= numberInt; i++)
+{
+    double answer = Math.Pow(i, 3);
+    System.Console.Write($"{answer} ");
+}
