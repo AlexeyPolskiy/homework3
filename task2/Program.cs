a98b93fa4dafd0f741b1fd63f01eeaf95bba93c0
AlexeyPolskiy <flyouss@mail.ru>
2022-06-18 13:41:34 +0500

    two commit

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Напишите программу, которая принимает на
 // вход координаты двух точек и находит расстояние между
 // ними в 3D пространстве.
+
+int prompt(string message)
+{
+    System.Console.Write(message);
+    string number = Console.ReadLine();
+    int numberInt = int.Parse(number);
+    return numberInt;
+}
+int degree()
+{
+
+}
+
+
+int numberIntAX = prompt("Введите координату X числа A -> ");
+int numberIntAY = prompt("Введите координату Y числа A -> ");
+int numberIntAZ = prompt("Введите координату Z числа A -> ");
+int numberIntBX = prompt("Введите координату Х числа B -> ");
+int numberIntBY = prompt("Введите координату Y числа B -> ");
+int numberIntBZ = prompt("Введите координату Z числа B -> ");
+
+int answer = Math.Sqrt()
